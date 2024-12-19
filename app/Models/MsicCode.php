ce5a740996d614dace65443c637e273e75a4e772
Jsloo1129 <loojuinsong021129@gmail.com>
2024-12-19 21:31:10 +0800

    fix msic branch

--- app/Models/MsicCode.php
+++ app/Models/MsicCode.php
@@ -2,12 +2,9 @@
 
 namespace App\Models;
 
-use App\Models\Scopes\BranchScope;
-use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-#[ScopedBy([BranchScope::class])]
 class MsicCode extends Model
 {
     use HasFactory;
@@ -16,9 +13,5 @@ class MsicCode extends Model
     public function customer()
     {
         return $this->hasOne(Customer::class, 'msic_id');
-    }
-
-    public function branch() {
-        return $this->morphOne(Branch::class, 'object');
     }
 }
