6c44a81e3b7324fdfd55c263ad7d00e4eef744a3
yx13 <exian97@gmail.com>
2025-08-13 12:30:08 +0800

    update code

--- resources/views/customer/link.blade.php
+++ resources/views/customer/link.blade.php
@@ -6,20 +6,37 @@
             <x-app.page-title>{{ __('Customer Information') }}</x-app.page-title>
         </div>
         @include('components.app.alert.parent')
-        
+
         <div class="grid gap-y-8">
             @include('customer.form_step.info', [
-                'default_branch' => $default_branch
+                'default_branch' => $default_branch,
             ])
             @include('customer.form_step.location')
         </div>
+        @if (!isset($customer) || (isset($customer) && $customer->status != 3))
+            <div class="mt-8 flex justify-end">
+                <x-app.button.submit id="group-submit-btn">{{ __('Save and Update') }}</x-app.button.submit>
+            </div>
+        @endif
     </div>
 
-    <x-app.modal.create-customer-link-created-modal/>
+    <x-app.modal.create-customer-link-created-modal />
 @endsection
 
 @push('scripts')
-<script>
-    CUSTOMER = @json($customer ?? null);
-</script>
+    <script>
+        GROUP_SUBMITTING = false
+        CUSTOMER = @json($customer ?? null);
+
+        $('#group-submit-btn').on('click', function() {
+            if (GROUP_SUBMITTING) return
+            GROUP_SUBMITTING = true
+
+            $('#group-submit-btn').text('Updating')
+            $('#group-submit-btn').removeClass('bg-yellow-400 shadow')
+            $('.err_msg').addClass('hidden') // Remove error messages
+
+            $('#info-form').submit()
+        })
+    </script>
 @endpush
