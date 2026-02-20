d34fe463a5e7304ac4c987ca4b0e784a9a181b35
yx13 <exian97@gmail.com>
2026-02-20 13:29:57 +0800

    feat: Add PDF preview mode for quotations in approval workflow
    Split the PDF viewing logic to distinguish between normal viewing and preview
    mode. When a quotation is in approval pending or rejected status, the PDF is
    opened with hidden toolbar and navigation panes for a cleaner preview experience.
    
    Changes:
    - Refactored `can_view_pdf` flag to always allow viewing non-draft quotations
    - Added new `is_preview_pdf` flag to track approval workflow states
    - Updated quotation form to append PDF parameters (#toolbar=0&navpanes=0) for
      approval pending/rejected statuses
    - Updated quotation list PDF links to conditionally include toolbar parameters
    - Added MFG directory to .gitignore for manufacturing files
    
    ---
    Files changed:
    - .gitignore: Added MFG directory to ignore list
    - app/Http/Controllers/SaleController.php: Split PDF viewing logic into two flags
    - resources/views/quotation/form.blade.php: Added conditional toolbar parameters
    - resources/views/quotation/list.blade.php: Updated PDF link generation

--- resources/views/quotation/form.blade.php
+++ resources/views/quotation/form.blade.php
@@ -264,7 +264,11 @@
 
                     // If save and preview, open PDF in new tab
                     if (isSaveAndPreview == 'true' && res.data != undefined && res.data.sale) {
-                        window.open(`{{ config('app.url') }}/quotation/pdf/${res.data.sale.id}`, '_blank')
+                        let pdfUrl = `{{ config('app.url') }}/quotation/pdf/${res.data.sale.id}`
+                        if (res.data.sale.status == {{ App\Models\Sale::STATUS_APPROVAL_PENDING }} || res.data.sale.status == {{ App\Models\Sale::STATUS_APPROVAL_REJECTED }}) {
+                            pdfUrl += '#toolbar=0&navpanes=0'
+                        }
+                        window.open(pdfUrl, '_blank')
                     }
 
                     setTimeout(() => {
